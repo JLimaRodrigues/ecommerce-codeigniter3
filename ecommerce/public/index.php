7b7602211964d951d5f37ad0624c4dafcf000a3b
lima.rodrigues <limarodrigues.silva@eb.mil.br>
2025-07-18 17:02:13 -0300

    feat: Agora com CodeIgniter 3

--- ecommerce/public/index.php
+++ ecommerce/public/index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo "Ola mundo"; exit;
+$system_path = '../ci/system/';
+$application_folder = '../ci/application/';
+
+define('BASEPATH', str_replace("\\", "/", $system_path));
+define('APPPATH', str_replace("\\", "/", $application_folder . '/'));
+define('VIEWPATH', APPPATH . 'views/');
+define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+
+require_once BASEPATH . '/core/CodeIgniter.php';
